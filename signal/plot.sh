c9d2d32d3e5be20a02895cb7e80d702a44847af0
G1gg1L3s <mr.a678i@gmail.com>
2021-02-05 17:18:20 +0200

    Add plotting

---
+++ signal/plot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Plot the data in the FILE with labels X and Y on axes into the OUT.png file
+FILE=$1
+X=$2
+Y=$3
+OUT=$4
+
+if test "$#" -ne 4; then
+    echo "Usage: $0 <file> <x-label> <y-label> <output.png>"
+    exit 1
+fi
+
+gnuplot -p -e "                                                            \
+    set autoscale;                                                         \
+    set grid ytics lt 0 lw 1 lc rgb '#bbbbbb';                             \
+    set grid xtics lt 0 lw 1 lc rgb '#bbbbbb';                             \
+    set xlabel '${X}';                                                     \
+    set ylabel '${Y}';                                                     \
+    set term png;                                                          \
+    set output '${OUT}';                                                   \
+    set terminal png size 1920,1080;                                       \
+    plot '${FILE}' with linespoints pointtype 7 pointsize 0.5;             \
+"
